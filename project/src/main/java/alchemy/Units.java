8d06387a1cc5e7258bd2b2f42e249212e93c5a21
joran <jorannaessens@gmail.com>
2026-04-22 13:52:19 +0200

    enum met de eenheden en project root gemaakt

---
+++ project/src/main/java/alchemy/Units.java
@@ -0,0 +1,11 @@
+package src.main.java.alchemy;
+
+public enum Units {
+        DROP(1),
+        SPOON(8),
+        VIAL(40),
+        BOTTLE(120),
+        JUG(840),
+        BARREL(10080),
+        STOREROOM(50400);
+}
