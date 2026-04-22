9d9b026657cb174e362f8b5acdd8d66a578cfa21
joran <jorannaessens@gmail.com>
2026-04-22 11:00:16 +0200

    nieuwe klasse

---
+++ project/src/main/java/alchemy/IngredientType.java
@@ -0,0 +1,13 @@
+/**
+ * @author Joran Naessens
+ * @author Maxim Samyn
+ * @author Lars Debrabander
+ * @param simpleName
+ * @return
+ */
+public class IngredientType extends AlchemicIngredient {
+    private String[] State = {"Liquid","Powder"};
+
+
+
+}
