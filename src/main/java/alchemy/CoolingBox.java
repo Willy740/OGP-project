b96ea5b10b02b2d2da069f960281eaaffd566568
joran <jorannaessens@gmail.com>
2026-05-11 17:50:42 +0200

    basic implementation of CoolingBox

--- src/main/java/alchemy/CoolingBox.java
+++ src/main/java/alchemy/CoolingBox.java
@@ -1,4 +1,23 @@
 package alchemy
-public class CoolingBox {
-    public long targetColdness;
+public class CoolingBox extends Device{
+    private long targetColdness;
+
+    public CoolingBox(long targetColdness) {
+        this.targetColdness = targetColdness;
+    }
+
+    public setColdness(long target){
+        this.targetColdness = target;
+    }
+
+    public executeOperation(){
+        private long hotness = Temperature.getHotness();
+        private long coldness = Temperature.getColdness();
+        if (coldness == 0){
+            Temperature.cool(hotness + this.targetColdness)
+        }
+        else{
+            Temperature.cool(this.targetColdness - coldness)
+        }
+    }
 }
