f4374f0094e558b1d175b5cdc4a6aa567208eb30
joran <jorannaessens@gmail.com>
2026-05-14 10:59:11 +0200

    implemented MixtureIngredientType

--- src/main/java/alchemy/MixtureIngredientType.java
+++ src/main/java/alchemy/MixtureIngredientType.java
@@ -1,18 +1,55 @@
 package Achemy;
 
-public class MixtureIngredientType{
+public class MixtureIngredientType extends IngredientType{
 
+    public MixtureIngredientType(Set<IngredientType> types){
+        super(new MixtureIngredientName(types), calculateDefaulatState(types), calculateDefaultTemperature(types) )
+    }
 
     public boolean isMixture(){
         return true;
     }
 
     public String getSpecialName(){
-
+        return ((MixtureIngredientName) getName()).getSpecialName;
     }
 
     public void setSpecialName(String specialName){
+        ((MixtureIngredientName) getName()).setSpecialName(specialName);
+    }
 
+    public State calculateDefaultState(Set<IngredientType> types){
+        IngredientType best = null;
+        for(IngredientType type : types){
+            if (best == null){
+                best = type;
+            }
+            else if (type.distanceToInterval() < best.distanceToInterval()){
+                best = type;
+            }
+            else if (type.distanceToInterval() == best.distanceToInterval()){
+                if (type.getDefaultState() == State.LIQUID){
+                    best = type;
+                }
+            }
+        }
+        return best.getDefaultState();
+    }
+
+    public Temperature calculateDefaultTemperature(Set<IngredientType> types){
+        IngredientType best == null;
+        for(IngredientType type : types) {
+            if (best == null) {
+                best = type;
+            } else if (type.distanceToInterval() < best.distanceToInterval()) {
+                best = type;
+            } else if (type.distanceToInterval() == best.distanceToInterval()) {
+                if (type.getDefaultTemperature().getHotness > best.getDefaultTemperature.getHotness) {
+                    best = type;
+                }
+            }
+        }
+        return best.getDefaultTemperature();
     }
 
 }
