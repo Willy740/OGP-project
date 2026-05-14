b550b3667d9d54e1e04bb5494a5ded84749b9ec2
joran <jorannaessens@gmail.com>
2026-05-14 07:53:28 +0200

    made a map with all the old mixture classes

--- src/main/java/alchemy/IngredientContainer.java
+++ src/main/java/alchemy/IngredientContainer.java
@@ -6,9 +6,40 @@ package alchemy;
  */
 public class IngredientContainer {
 
-    private Unit capacity;
+    private final UNIT capacity;
 
     private AlchemicIngredient content;
 
-    private boolean destroyed;
+    private boolean destroyed = false;
+
+    public IngredientContainer(UNIT capacity, AlchemicIngredient content, boolean destroyed) {
+        this.capacity = capacity;
+        this.content = content;
+        this.destroyed = destroyed;
+    }
+
+    public UNIT getCapacity() {
+        return this.capacity;
+    }
+
+    public AlchemicIngredient getContent() {
+        return this.content;
+    }
+
+    public boolean isEmpty() {
+        if (getCapacity() == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isDestroyed() {
+        return this.destroyed;
+    }
+
+    public void empty() {
+        this.capacity = null;
+        this.content = null;
+        this.destroyed = true;
+    }
 }
--- src/main/java/alchemy/Kettle.java
+++ src/main/java/alchemy/Kettle.java
@@ -3,6 +3,5 @@ package alchemy
 public class Kettle extends Device{
 
     public void executeOperation(){
-
     }
 }
