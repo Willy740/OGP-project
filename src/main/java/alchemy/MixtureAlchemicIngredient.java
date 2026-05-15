fa8fbb1ce2f7c044937377cf9b2cdadcba1574fa
joran <jorannaessens@gmail.com>
2026-05-15 09:49:49 +0200

    implemented MixtureIngredientType

--- src/main/java/alchemy/MixtureAlchemicIngredient.java
+++ src/main/java/alchemy/MixtureAlchemicIngredient.java
@@ -1,18 +1,22 @@
 package Alchemy;
 
-public class MixtureAlchemicIngredient{
+public class MixtureAlchemicIngredient extends AlchemicIngredient{
     
     
     public String getFullName(){
-        
+        return getMixturetype().getFullName(getTemperature(), getType().getDefaultTemperature());
     }
     
     public String getSpecialName(){
-        
+        return getMixturetype().getFullName(getTemperature(), getType().getDefaultTemperature());
     }
-    
+
     public void setSpecialName(String specialName){
-    
+        getMixtureType().setSpecialName(specialName);
+    }
+
+    public MixtureIngredientType getMixtureType(){
+        return (MixtureIngredientType) getType();
     }
     
 }
